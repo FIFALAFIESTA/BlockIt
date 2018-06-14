4a83f53bd0418cb2b0b5d6696627a13b81be6542
tynchtyk <tynchtykkaldybaev@gmail.com>
2018-06-14 18:56:40 +0900

    merge

--- app/src/main/java/com/example/akylbektokonuulu/blockit/Notifications.java
+++ app/src/main/java/com/example/akylbektokonuulu/blockit/Notifications.java
@@ -62,7 +62,18 @@ public class Notifications extends AppCompatActivity {
     }
 
 
+    class NotificationReceiver extends BroadcastReceiver {
 
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            //History.data.add(new history_entry("t", "y", "n", "t", "y", "k"));
+            String stringExtra = intent.getStringExtra("notification_event");
+            history_entry tmp = History.parse(stringExtra);
+            History.data.add(tmp);
+            String temp = intent.getStringExtra("notification_event") + "\n" + txtView.getText();
+            txtView.setText(temp);
+        }
+    }
     public void buttonClicked(View v){
 
         if(v.getId() == R.id.btnCreateNotify){
